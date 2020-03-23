c05063f824d95b8cc4564efafa1c42513fb5ee24
MichaelRedM <michael.rudolph@redmedical.de>
2020-03-23 14:49:00 +0100

    feat(ci): lol

---
+++ .github/workflows/read-changelog.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = process.env['CHANGELOG_PATH'] || './CHANGELOG.md';
+const version = process.env['VERSION'];
+
+const changelogLines = fs.readFileSync(path).toString().split('\n');
+
+let result = [];
+
+let logRegionStarted = false;
+
+for (let i = 0; i < changelogLines.length; i++) {
+    if (changelogLines[i].startsWith('## ' + version) || changelogLines[i].startsWith('## [' + version)) {
+        logRegionStarted = 'Major';
+    } else if (changelogLines[i].startsWith('### ' + version) || changelogLines[i].startsWith('### [' + version)) {
+        logRegionStarted = 'Minor';
+    } else if (changelogLines[i].startsWith('## ')) {
+        logRegionStarted = false;
+        break;
+    } else if (
+        changelogLines[i].startsWith('### ') &&
+        changelogLines[i].indexOf('Bug Fixes') === -1 &&
+        changelogLines[i].indexOf('Features') === -1 &&
+        changelogLines[i].indexOf('BREAKING CHANGES') === -1
+    ) {
+        logRegionStarted = false;
+        break;
+    }
+    if (logRegionStarted) {
+        result.push(changelogLines[i]);
+    }
+}
+
+console.log(result.join('\n'));
